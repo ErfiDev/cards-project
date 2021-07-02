3d7b7b3fbcbd23d9d5678109715ea8b69033349f
Erfan <erfanhanifezade@gmail.com>
2021-07-02 15:02:33 +0430

    arr and slice practice

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	now := time.Now()
+	PrintDate(now.Year(), now.Day(), int(now.Month()))
+
+	floting := 3.14
+	toInt := int(floting)
+	fmt.Printf("\n %d \n", toInt)
+
+	randomNum := math.Pi
+	fmt.Println(randomNum)
+
+	// Arr := []string;
+	// var Arr []string
+	// var i int
+	// for i = 0; i <= 5; i++ {
+	// 	input := ""
+	// 	fmt.Scanf("%s", &input)
+	// 	Arr = append(Arr, input)
+	// }
+	// fmt.Println(Arr)
+	Arr := []string{"erfan", "ali", "reza"}
+	fmt.Println(len(Arr), "length")
+}
+
+func PrintDate(year, day, month int) {
+	fmt.Printf("%d", year)
+	fmt.Printf("%d", month)
+	fmt.Printf("%d", day)
 }
