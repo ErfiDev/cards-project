8fb5994fba48e4196825f807c912b4409a030172
Erfan <erfanhanifezade@gmail.com>
2021-07-04 20:50:43 +0430

    update main.goo

--- main.go
+++ main.go
@@ -26,8 +26,29 @@ func main() {
 	// 	Arr = append(Arr, input)
 	// }
 	// fmt.Println(Arr)
-	Arr := []string{"erfan", "ali", "reza"}
-	fmt.Println(len(Arr), "length")
+
+	//this is a slice
+	Slice := []int{}
+	Slice = append(Slice, 10)
+	Slice = append(Slice, 15)
+	Slice = append(Slice, 20)
+	Slice = append(Slice, 25)
+	Slice = append(Slice, 30)
+	fmt.Println(Slice)
+
+	//this is a array
+	Arr := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(Arr)
+
+	var Arr2 = [2]int{1, 2}
+	fmt.Println(Arr2)
+
+	var sliceVar []int
+	sliceVar = append(sliceVar, 10)
+
+	for i := 0; i < 10; {
+		fmt.Println(i)
+	}
 }
 
 func PrintDate(year, day, month int) {
