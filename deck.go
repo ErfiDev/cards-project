e6aa64ed651e171f9b6d29a33b06bbae6c0f2874
Erfan <erfanhanifezade@gmail.com>
2021-07-16 18:26:16 +0430

    working with struct and map

--- deck.go
+++ deck.go
@@ -25,3 +25,7 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+func deal(d deck, handSize int) {
+	fmt.Println(d)
+}
--- main.go
+++ main.go
@@ -3,4 +3,13 @@ package main
 func main() {
 	cards := newDeck()
 	cards.print()
+	deal(cards, 10)
+	
+	newTenCar := createRandomCar(10);
+	newTenCar.print()
+
+
+	// newMap := make(map[string]bool);
+	// newMap["single?"] = true;
+	// fmt.Printf("\n %v \n" , newMap);
 }
