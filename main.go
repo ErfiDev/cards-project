24ac755ccd9a12e379a580642b5e1a26c0a79894
Erfan <erfanhanifezade@gmail.com>
2021-07-17 14:05:44 +0430

    for loop practice

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
-	cards.print()
-	deal(cards, 10)
-	
-	newTenCar := createRandomCar(10);
-	newTenCar.print()
+	// cards := newDeck()
+	// cards.print()
+	// deal(cards, 10)
+	condition := true;
+	var i int = 0;
+	for condition {
+		i++
+		fmt.Println(i)
+		if i == 15 {
+			break
+		}
+	}
+
+
+	// newTenCar := createRandomCar(10);
+	// newTenCar.print()
 
 
 	// newMap := make(map[string]bool);
