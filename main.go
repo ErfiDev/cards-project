eba0145b9bdc51a5b6d35687f652875d0ff307b8
Erfan <erfanhanifezade@gmail.com>
2021-07-18 16:25:53 +0430

    dec type

--- main.go
+++ main.go
@@ -1,30 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"time"
-)
-
 func main() {
 	cards := newDeck()
 	cards.print()
-
-	newCars := createRandomCar(3)
-	newCars.print()
-
-	day := time.Now().Day()
-	fmt.Printf("%d day \n", day)
-
-	humans := human{}
-	humans = humans.addNew("erfan", "hanifezade", "developer", 17)
-	humans = humans.addNew("maziar", "rezaee", "mechanic", 22)
-	fmt.Println(humans)
-
-	result, err := os.ReadFile("data.json")
-	if err != nil {
-		fmt.Println("error")
-	}
-
-	os.Stdout.Write(result)
 }
