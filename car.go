eb1db814452a8426ad63bcd3a62a9538422dbe00
Erfan <erfanhanifezade@gmail.com>
2021-07-18 16:24:26 +0430

    init carType branch

--- car.go
+++ car.go
@@ -6,38 +6,43 @@ import (
 )
 
 type carStruct struct {
-	name string
+	name  string
 	color string
-} 
+}
 type car []carStruct
 
-func createCar(name , color string) car {
+func createCar(name, color string) car {
 	newModel := carStruct{
-		name: name,
+		name:  name,
 		color: color,
 	}
 
-	newCar := car{newModel};
-	return newCar;
+	newCar := car{newModel}
+	return newCar
 }
 
 func createRandomCar(count int) car {
-	names := []string{"opel","bmw","dodge","mustang","honda"};
-	colors := []string{"blue","white","red","orange","yellow"};
+	names := []string{"opel", "bmw", "dodge", "mustang", "honda"}
+	colors := []string{"blue", "white", "red", "orange", "yellow"}
 
-	newCar := car{};
+	newCar := car{}
 
-	for i := 0 ; i <= count ; i++ {
-		random := rand.Intn(5);
+	for i := 0; i <= count; i++ {
+		randomForName := randomNum(5)
+		randomForColor := randomNum(5)
 		newCar = append(newCar, carStruct{
-			name : names[random],
-			color: colors[random],
-		})	
+			name:  names[randomForName],
+			color: colors[randomForColor],
+		})
 	}
 
 	return newCar
 }
 
 func (c car) print() {
-	fmt.Println(c);
+	fmt.Println(c)
+}
+
+func randomNum(index int) int {
+	return rand.Intn(index)
 }
