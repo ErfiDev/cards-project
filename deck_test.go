0fb83ea29fbe05f430786b06ec0a640a2edaef42
Erfan <erfanhanifezade@gmail.com>
2021-07-25 14:01:43 +0430

    shuffle func test

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,22 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	deck := newDeck()
+	deck.shuffle()
+
+	if len(deck) != 16 {
+		t.Errorf("Expected shuffled deck length of 16, but got %v", len(deck))
 	}
 }
