3dee2eb01f00817fa672b2f88352fd332472510d
Erfan <erfanhanifezade@gmail.com>
2021-07-25 14:13:56 +0430

    use all methods

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeckFromFile("data.txt")
+	cards := newDeck()
 	cards.print()
+
+	before, after := deal(cards, 4)
+	fmt.Println("dealed cards : ", before, " ", after)
+
+	after.shuffle()
+	after.saveToFile("data.txt")
+
+	after.print()
+
+	deckFromFile := newDeckFromFile("data.txt")
+	deckFromFile.print()
 }
