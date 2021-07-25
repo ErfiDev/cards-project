dbbda9cc559fdd6464d7236d83a70a8d90222f40
Erfan <erfanhanifezade@gmail.com>
2021-07-25 14:09:01 +0430

    save to file test pass

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -19,4 +20,18 @@ func TestShuffle(t *testing.T) {
 	if len(deck) != 16 {
 		t.Errorf("Expected shuffled deck length of 16, but got %v", len(deck))
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktest")
+
+	deck := newDeck()
+	deck.saveToFile("_decktest")
+
+	readTestFile := newDeckFromFile("_decktest")
+	if len(readTestFile) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(readTestFile))
+	}
+
+	os.Remove("_decktest")
 }
